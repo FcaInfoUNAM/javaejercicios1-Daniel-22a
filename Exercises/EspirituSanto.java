2b8f81e705fdb94f587f02c72f98000ff5ebd48b
Daniel-Diaz <111396077+Daniel-22a@users.noreply.github.com>
2024-08-24 01:30:03 +0000

    .

---
+++ Exercises/EspirituSanto.java
@@ -0,0 +1,8 @@
+public class EspirituSanto extends Hijo {
+    String apellidoReal;
+    public EspirituSanto(String nombre, String apellido, String apellidoReal){
+        super(nombre, apellido);
+        this.apellidoReal = apellidoReal;
+    }
+
+}
